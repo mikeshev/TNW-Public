dd77b543b0ba3b713b65070c3f4e6f3766fa9afe
Mike Shevenell <mikeshev@users.noreply.github.com>
2023-08-29 09:53:45 -0400

    Add files via upload

--- Tools/Get-Ext-Temp.ps1
+++ Tools/Get-Ext-Temp.ps1
@@ -2,7 +2,7 @@
         	$DefaultExtPath = 'AppData\Local\Google\Chrome\User Data\Default\Extensions'
         	$ProfilesExtPath = 'AppData\Local\Google\Chrome\User Data\Profile *'
 
-	  	# Get Default user profiles that contain this a Chrome extensions folder
+	  	# Set Default & Optional user profiles that contain a Chrome extensions folder
                 $DefaultPath = Join-path -path "fileSystem::\\$Computer\C$\Users\*" -ChildPath $DefaultExtPath
 		$ProfilePath = Join-path -Path "fileSystem::\\$Computer\C$\Users\*" -ChildPath $ProfileExtPath
 
@@ -11,11 +11,11 @@
 		$Paths += $DefaultPath
 
 		Get-ChildItem $ProfilePath -Filter "Profile *" -Directory | %{$_.fullname} | ForEach-Object {
+echo $_
 		    $Paths += $_
 		}
 
 		ForEach ($ExtPath in $Paths){
-		  echo $ExtPath
                   Get-Item -Path $ExtPath -ErrorAction SilentlyContinue | ForEach-Object{
                     $Extensions += Get-ChildItem -Path $_ -Directory -ErrorAction SilentlyContinue
                   }
